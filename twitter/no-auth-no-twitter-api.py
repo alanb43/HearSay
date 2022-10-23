0a00575db8d43d8bf8cba693671337996ff38239
JCUN23 <jcun@umich.edu>
2022-10-23 01:21:37 -0400

    way better method no authentication or developer portal needed and no limits

---
+++ twitter/no-auth-no-twitter-api.py
@@ -0,0 +1,18 @@
+import snscrape.modules.twitter as twitter
+import pandas as pd
+
+author = "FabrizioRomano"
+topic = "Cristiano Ronaldo"
+include_replies = False
+num_tweets = 100
+
+query = f"""(from:{author}) {topic} -filter:replies -filter:retweets"""
+tweets = []
+
+for tweet in twitter.TwitterSearchScraper(query).get_items():
+    if len(tweets) == num_tweets:
+        break
+    tweets.append([tweet.date, tweet.user.username, tweet.content])
+
+df = pd.DataFrame(tweets, columns=['Date', 'Author', 'Tweet'])
+print(df)
