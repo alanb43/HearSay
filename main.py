3d6ae5d874469471be862919064e14b687c05700
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-10-27 02:04:10 -0400

    Went through and manually integrated the changes I made on the flask_and_integration branch
    This made the code work functionally on a very basic level, it can now fully run and be used by a user although it isn't great

--- main.py
+++ main.py
@@ -7,35 +7,45 @@ import numpy as np
 
 def main():
     """Integrates systems to allow an end-to-end interaction."""
-    #speech_manager = SpeechManager()
+    speech_manager = SpeechManager()
     input_analyzer = InputAnalyzer()
     tweet_snagger = TweetSnagger()
     sentiment_analyzer = SentimentClassifier()
     while True:
         try:
-            #utterance = speech_manager.speech_to_text()
-            utterance = input("Provide a sentence: ")
-            if utterance=="q":
+            # Decide whether to take speech or text input
+            talk_or_text = input('Would you like to SPEAK or TYPE a phrase (or QUIT)? (s/t/q): ')
+            if talk_or_text == 'q':
                 break
+            elif talk_or_text == 's':
+                utterance = speech_manager.speech_to_text()
+            else:
+                utterance = input('Input phrase: ')
             analysis = input_analyzer.analyze(utterance)
             
+            # Get intents and entities
             intents = analysis["intents"]
             entities = analysis["entities"]
+
             # Determine primary intent
             max_score = np.argmax(np.array(intents["scores"]))
-            if intents["scores"][max_score] > 0.5:
+            if intents["scores"][max_score] > 0.3:
                 primary_intent = intents["labels"][max_score]
             else:
                 primary_intent = "other"
-            # Determine best entities / targets?
 
-            # TODO: add all entities to topics
+            # Determine entities/targets
+            entity_words = [entity['word'] for entity in entities]
+
+            # TODO: add all entities to topics and customize for intents
+            # Deal with known intents
             if primary_intent == "injury":
-                tweets = tweet_snagger.snag_tweets([entities[0]["word"], "injury"])
+                tweets = tweet_snagger.snag_tweets(topics=entity_words + ["injury"], num_tweets=1)
             elif primary_intent == "trade":
-                tweets = tweet_snagger.snag_tweets([entities[0]["word"], "trade"])
-            elif primary_intent == "other":
-                tweets = tweet_snagger.snag_tweets([entities[0]["word"]], num_tweets = 100)
+                tweets = tweet_snagger.snag_tweets(topics=entity_words + ["trade"], num_tweets=1)
+            # Deal with unknown intents TODO: Update this, I didn't update with the rest of the file
+            else: 
+                tweets = tweet_snagger.snag_tweets(entity_words, num_tweets = 100)
                 output = np.zeros(3) # 0 - positive, 1 - neutral, 2 - negative
                 for tweet in tweets:
                     sentiment = sentiment_analyzer.analyze(tweet["content"])
@@ -54,19 +64,13 @@ def main():
                 elif idx == 2:
                     print(entities[0]["word"] + " has a negative sentiment")
 
-
-
-
-
-            # print(tweets)
-            
-            # Do something with the tweets, turn it into English that can be spoken
-            response = ""
-            # Speech output
-            #speech_manager.text_to_speech(response)
-
-        except:
-            print("Exception occurred")
+            # Speech/text output
+            response = tweets[0]['content']
+            print ('Response tweet:', response)
+            speech_manager.text_to_speech(response)
+        
+        except Exception as e:
+            print("Exception occurred:", e)
 
 
 if __name__ == "__main__":
