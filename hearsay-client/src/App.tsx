125f0bb175388305492977aee4dc0042b0b58c40
jcun <jcun@umich.edu>
2022-10-28 00:08:25 -0400

    added functions to prepare for integrating with backend

--- hearsay-client/src/App.tsx
+++ hearsay-client/src/App.tsx
@@ -6,8 +6,20 @@ const App: React.FC = () => {
   const [query, setQuery] = useState('');
   const [response, setResponse] = useState('');
 
+  const onMicClick = () => {
+    // need to call speech_manager.py
+    let speech_to_text = ""
+    setQuery(speech_to_text);
+  }
+
+  const onSubmit = () => {
+    // call backend - generate response and display on page
+    let response = "";
+    setResponse(response);
+  }
+
   let responseDiv = <div></div>
-  if (response != '') {
+  if (response !== '') {
     responseDiv = <div id="responseDiv">
       {response}
     </div>
@@ -21,8 +33,12 @@ const App: React.FC = () => {
       </div>
       <h3>Rumor has it we've got all the answers you need.</h3>
       <div id="search-bar-section">
-        <input id="search-bar" placeholder="Type a question, or click the microphone and ask one!" value={query} onChange={e => setQuery(e.target.value)}/>
-        <div id="mic-button"><img id="mic-img" src="mic.png" alt="" /></div>
+        <input id="search-bar"
+        placeholder="Type a question, or click the microphone and ask one!" 
+        value={query} 
+        onChange={e => setQuery(e.target.value)}
+        onSubmit={onSubmit}/>
+        <div id="mic-button" onClick={onMicClick}><img id="mic-img" src="mic.png" alt="" /></div>
       </div>
       {responseDiv}
     </div>
