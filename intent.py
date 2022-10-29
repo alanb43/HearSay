4ebd54e65f9c6d5b0840c9c10f420c927b736e45
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-10-29 15:24:03 -0400

    Fixed english tweet query and made intents better, application works much better overall

--- intent.py
+++ intent.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class Intent:
     """Intents for our """
-    TRADE: str = "trade"
-    INJURY: str = "injury"
+    TRADE: str = "trade OR headed OR sold OR transfer"
+    INJURY: str = "injury OR hurt OR benched"
 
 INTENTS = [Intent.TRADE, Intent.INJURY]
