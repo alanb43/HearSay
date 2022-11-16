c1773b6799b15ff0c6a12626abfedf2b9cfbda6d
Chetan Reddy <chereddy@umich.edu>
2022-11-15 22:38:00 -0500

    began creating train file for sentiment analysis

---
+++ sentiment-analysis/train.py
@@ -0,0 +1,44 @@
+from transformers import TrainingArguments, Trainer, AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification
+import numpy as np
+from datasets import load_metric, load_dataset
+import torch
+
+def compute_metrics(eval_pred):
+   load_accuracy = load_metric("accuracy")
+   load_f1 = load_metric("f1")
+  
+   logits, labels = eval_pred
+   predictions = np.argmax(logits, axis=-1)
+   accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
+   f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
+   return {"accuracy": accuracy, "f1": f1}
+
+
+dataset = load_dataset("csv", data_files="dataset.csv")
+
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest")
+model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment-latest", labels = 3)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+repo_name = "finetune-sentiment-model-players-teams"
+
+training_args = TrainingArguments(
+   output_dir=repo_name,
+   learning_rate=2e-5,
+   per_device_train_batch_size=16,
+   per_device_eval_batch_size=16,
+   num_train_epochs=2,
+   weight_decay=0.01,
+   save_strategy="epoch",
+   push_to_hub=False,
+)
+ 
+trainer = Trainer(
+   model=model,
+   args=training_args,
+   train_dataset=tokenized_train,
+   eval_dataset=tokenized_test,
+   tokenizer=tokenizer,
+   data_collator=data_collator,
+   compute_metrics=compute_metrics,
+)
