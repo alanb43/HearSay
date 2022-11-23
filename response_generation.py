367d84bc043a109a9ae62e71fbb8ed7a566af22e
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-11-23 18:10:22 -0500

    Updated response generation class to include private functions

--- response_generation.py
+++ response_generation.py
@@ -8,3 +8,15 @@ class ResponseGenerator:
 
     def generate_response(self, qa_input):
         return self.nlp(qa_input)
+
+    def __summarize_text(self, text_input: str) -> str:
+        return 'Summarized Text'
+
+    def __get_relevant_tweets(self, tweets_input: list[str]) -> list[str]:
+        return ["Relevant Tweet"]
+
+    def __get_question_answer(self, question: str, answer: str) -> str:
+        return "Answer to Question"
+
+    def __get_sentiment_analysis(self, tweets_input: list[str]) -> str:
+        return "Sentiment"
