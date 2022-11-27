1c9cdf5b4000818ec86039352c064910df80e32b
Frankie Lizcano <lizcano.frankie@gmail.com>
2022-11-26 20:52:53 -0500

    Added summarizer class that will take in an array of tweet_snagger json tweets and summarize them

---
+++ summarizer.py
@@ -0,0 +1,19 @@
+from transformers import pipeline
+
+class Summarizer:
+
+    def __init__(self):
+        self.model = pipeline("summarization", model="knkarthick/MEETING_SUMMARY")
+
+    def summarize_tweets(self, tweets):
+        tweets_paragraph = self._combine_tweets(tweets)
+        #print("TWEETS PARAGRAPH \n\n", tweets_paragraph)
+        return self.model(tweets_paragraph[0:1024])
+        
+    def _combine_tweets(self, tweets):
+        tweets_paragraph = ""
+        for tweet in tweets:
+            tweets_paragraph += tweet["content"] + " "
+        return tweets_paragraph
+
+
