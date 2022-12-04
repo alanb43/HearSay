8a0bf1c25a298787161f54e09654f8ebd8d7fb92
alanb43 <ab43work@gmail.com>
2022-12-04 01:22:05 -0500

    fixed functional testing syntax (missing 'it')

--- hearsay-client/cypress/e2e/functional_integ.cy.js
+++ hearsay-client/cypress/e2e/functional_integ.cy.js
@@ -1,32 +1,51 @@
 /// <reference types="cypress" />
 
 describe('HearSay q&a functional test', () => {
-  cy.visit('localhost:3000');
+  it('tests the injured intent', () => {
+    cy.visit('localhost:3000');
+  
+    // testing injury intent
+    cy.get('#search-bar')
+      .type('Are any of the LA Lakers injured?')
+      .should('have.value', 'Are any of the LA Lakers injured?');
+    
+    cy.get('#submit-button')
+      .click();
+    
+    cy.wait(8000); 
+    // response generation: sentiment
+    cy.get('.HearSay')
+      .eq(0)
+      .contains('The overall sentiment is');
+  });
+  
+  it('tests the question and answering intent', () => {
+    // testing intent-less question + answering
+    cy.get('#search-bar')
+      .type('Is Lionel Messi the greatest soccer player ever?');
+    
+    cy.get('#submit-button')
+      .click();
+    
+    cy.wait(8000);
+    // response generation: short answer 
+    cy.get('.HearSay')
+      .eq(1)
+      .contains('The short answer is');
+  });
 
-  // testing injury intent
-  cy.get('#search-bar')
-    .type('Are any of the LA Lakers injured?')
-    .should('have.value', 'Are any of the LA Lakers injured?');
-  
-  cy.get('#submit-button')
-    .click();
-  
-  cy.wait(10000); 
-  // response generation: sentiment
-  cy.get('.HearSay')
-    .eq(0)
-    .contains('The overall sentiment is');
-  
-  // testing intent-less question + answering
-  cy.get('#search-bar')
-    .type('Is Lionel Messi the greatest soccer player ever?');
-  
-  cy.get('#submit-button')
-    .click();
-  
-  cy.wait(10000);
-  // response generation: short answer 
-  cy.get('.HearSay')
-    .eq(1)
-    .contains('The short answer is');
+  it('tests the trade intent', () => {
+    // testing intent-less question + answering
+    cy.get('#search-bar')
+      .type('Should LeBron get traded to the Detroit Pistons?');
+    
+    cy.get('#submit-button')
+      .click();
+    
+    cy.wait(10000);
+    // response generation: short answer 
+    cy.get('.HearSay')
+      .eq(2)
+      .contains('The most relevant tweet is');
+  });
 });
