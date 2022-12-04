12d797fd3846f8747f17faf4973f1521512c1a35
alanb43 <ab43work@gmail.com>
2022-12-04 01:13:45 -0500

    Removed profile manager for deployed HearSay, needs re-work for multi-user expansion (was only designed for local use). Rewrote smoke and functional tests to not make use of profiles.

--- hearsay-client/cypress/e2e/functional_integ.cy.js
+++ hearsay-client/cypress/e2e/functional_integ.cy.js
@@ -1,51 +1,32 @@
 /// <reference types="cypress" />
 
-describe('HearSay user profile setup functional test', () => {
+describe('HearSay q&a functional test', () => {
+  cy.visit('localhost:3000');
+
+  // testing injury intent
+  cy.get('#search-bar')
+    .type('Are any of the LA Lakers injured?')
+    .should('have.value', 'Are any of the LA Lakers injured?');
   
-  it('starts back-end profile creation using conversation', () => {  
-    cy.visit('localhost:3000');
-    
-    cy.get('#search-bar')
-      .type('I\'d like to create a profile')
-      .should('have.value', 'I\'d like to create a profile');
-
-    cy.get('#submit-button')
-      .click();
-
-    cy.wait(1000);
-    cy.get('.HearSay')
-      .should('have.text', 'What\'s your name?');
-  });
-
-  it('builds out user profile', () => {
-    cy.get('#search-bar')
-      .type('Alan');
-
-    cy.get('#submit-button')
-      .click();
-
-    cy.wait(1500);
-    cy.get('.HearSay').eq(1)
-      .should('have.text', 'Nice to meet you, Alan. Which teams do you support?');
-    
-    cy.get('#search-bar')
-      .type('Manchester United');
-
-    cy.get('#submit-button')
-      .click();
-    
-    cy.wait(1500);
-    cy.get('.HearSay').eq(2)
-      .should('have.text', 'Ah, a Manchester United fan I see. Have any favorite players?');
-
-    cy.get('#search-bar')
-      .type('LeBron James');
-
-    cy.get('#submit-button')
-      .click();
-    
-    cy.wait(1500);
-    cy.get('.HearSay').eq(3)
-      .should('have.text', 'Got it. I\'ve set up your profile!');
-  });
+  cy.get('#submit-button')
+    .click();
+  
+  cy.wait(10000); 
+  // response generation: sentiment
+  cy.get('.HearSay')
+    .eq(0)
+    .contains('The overall sentiment is');
+  
+  // testing intent-less question + answering
+  cy.get('#search-bar')
+    .type('Is Lionel Messi the greatest soccer player ever?');
+  
+  cy.get('#submit-button')
+    .click();
+  
+  cy.wait(10000);
+  // response generation: short answer 
+  cy.get('.HearSay')
+    .eq(1)
+    .contains('The short answer is');
 });
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sentiment_classifier import SentimentClassifier
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from response_generation import ResponseGenerator
-from profile_manager import ProfileManager
+# from profile_manager import ProfileManager
 from data_classes.intents import Intents
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ input_analyzer = InputAnalyzer()
 tweet_snagger = TweetSnagger()
 sentiment_analyzer = SentimentClassifier()
 response_generator = ResponseGenerator()
-profile_manager = ProfileManager(input_analyzer, sentiment_analyzer, tweet_snagger, response_generator)
+# profile_manager = ProfileManager(input_analyzer, sentiment_analyzer, tweet_snagger, response_generator)
 # First requestion breaks for some reason, so warming it up
 # response_generator.generate_response("null", [{"content":"null"}])
 
@@ -26,44 +26,45 @@ def generate_response(utterance: str) -> str:
         primary_intent, entities = analysis["primary_intent"], analysis["entities"]
         print(primary_intent)        
         # if user is in process of creating a profile
-        if profile_manager.create_profile_state.engaged:
-            response = profile_manager.build_profile(utterance)
-        # if user wants to create a profile
-        elif primary_intent == Intents.PROFILE:
-            if profile_manager.profile_exists():
-                response = "You already set up a profile!"
-            else:
-                profile_manager.create_profile_state.engaged = True
-                response = "What's your name?"
+        # if profile_manager.create_profile_state.engaged:
+        #     response = profile_manager.build_profile(utterance)
+        # # if user wants to create a profile
+        # elif primary_intent == Intents.PROFILE:
+        #     if profile_manager.profile_exists():
+        #         response = "You already set up a profile!"
+        #     else:
+        #         profile_manager.create_profile_state.engaged = True
+        #         response = "What's your name?"
         # profiled user checking on their favorite teams or player
-        elif "favorite" in utterance:
-            if not profile_manager.profile_exists():
-                response = "You haven't set up a profile!"
-            elif primary_intent == Intents.FAV_TEAMS:
-                response = profile_manager.get_info_about_favorites(True)
-            else:
-                response = profile_manager.get_info_about_favorites(False)
+        # if "favorite" in utterance:
+        #     if not profile_manager.profile_exists():
+        #         response = "You haven't set up a profile!"
+        #     elif primary_intent == Intents.FAV_TEAMS:
+        #         response = profile_manager.get_info_about_favorites(True)
+        #     else:
+        #         response = profile_manager.get_info_about_favorites(False)
         # all other cases managed by response generator
-        else:
-            if len(entities) == 0:
-                response = "I didn't capture that, would you mind trying again?"
-                return response
-            
-            if "injured" in utterance:
-                primary_intent = Intents.INJURY
-            elif "trade" in utterance:
-                primary_intent = Intents.TRADE
+        
+        if len(entities) == 0:
+            response = "I didn't capture that, would you mind trying again?"
+            return response
+        
+        if "injured" in utterance:
+            primary_intent = Intents.INJURY
+        elif "trade" in utterance:
+            primary_intent = Intents.TRADE
 
-            entity_words = [entity['word'] for entity in entities]
-            tweets = tweet_snagger.snag_tweets(topics=entity_words, intent=primary_intent, num_tweets=50)
-            response = response_generator.generate_response(utterance, tweets)
+        entity_words = [entity['word'] for entity in entities]
+        print(entity_words)
+        tweets = tweet_snagger.snag_tweets(topics=entity_words, intent=primary_intent, num_tweets=50)
+        response = response_generator.generate_response(utterance, tweets)
 
     except Exception as e:
         print("Exception occurred:", e)
         response = "No one's talking about this, why don't you tweet it?"
-        if profile_manager.create_profile_state.engaged:
-            profile_manager.reset_profile()
-            response = "I couldn't capture that. I've reset the profile."
+        # if profile_manager.create_profile_state.engaged:
+        #     profile_manager.reset_profile()
+        #     response = "I couldn't capture that. I've reset the profile."
 
     return response
 
